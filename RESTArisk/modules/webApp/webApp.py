39306573f652230de417aaa3cc31f29d9928631f
kovax <kovacsdanielhun@gmail.com>
2016-10-14 16:24:24 +0200

    WebApp files added

--- RESTArisk/modules/webApp/webApp.py
+++ RESTArisk/modules/webApp/webApp.py
@@ -5,14 +5,16 @@ from flask import Flask
 class WebApp:
 
     class __WebApp:
+        app = None
         def __init__(self):
-            pass
-
+            print("Creating app.. ")
+            self.app = Flask(__name__)
 
     instance = None
     def __init__(self):
         if not WebApp.instance:
             WebApp.instance = WebApp.__WebApp()
     def __getinstance__(self):
-        return self.instance
+        return self.instance.app
+
 
