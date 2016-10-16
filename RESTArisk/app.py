598089e151e33ef84564b2cc9703f20a74e783fb
kovax <kovacsdanielhun@gmail.com>
2016-10-16 13:40:15 +0200

    WebApp files added

--- RESTArisk/app.py
+++ RESTArisk/app.py
@@ -10,7 +10,7 @@ class App:
     flaskApp = None
 
     def __init__(self):
-        self.flaskApp = webApp.WebApp().__getinstance__()
+        self.flaskApp = webApp.WebApp().__getInstance__()
         self.flaskApp.run(host="0.0.0.0",port=5000)
 
 
--- RESTArisk/modules/webApp/webApp.py
+++ RESTArisk/modules/webApp/webApp.py
@@ -7,14 +7,13 @@ class WebApp:
     class __WebApp:
         app = None
         def __init__(self):
-            print("Creating app.. ")
             self.app = Flask(__name__)
 
     instance = None
     def __init__(self):
         if not WebApp.instance:
             WebApp.instance = WebApp.__WebApp()
-    def __getinstance__(self):
+    def __getInstance__(self):
         return self.instance.app
 
 
