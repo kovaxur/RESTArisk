b24c523a09d60ecd76606b083f33f758f0b97981
kovax <kovacsdanielhun@gmail.com>
2016-10-18 22:33:25 +0200

    PEP8

--- RESTArisk/modules/authentication/SSO.py
+++ RESTArisk/modules/authentication/SSO.py
@@ -1,7 +1,6 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import datetime
-from flask import redirect
 from flask import request
 from flask import session
 import hashlib
@@ -16,7 +15,6 @@ class SSO(Authentication):
     password = None
     scope = "basic+displayName"
     _tokens = None
-    redirectOnAuth = None
 
     def __init__(self):
         self.hostname = Settings.getConfigValue("oauth2","hostname")
@@ -25,12 +23,10 @@ class SSO(Authentication):
 
     # This will do a reauthenticate, if the users' session expires.
     def reauthenticate(self):
-        data = {}
-        data['grant_type'] = 'refresh_token'
-        data['refresh_token'] = session['refresh_token']
-        ch = self.curlExec("oauth2/token", data)
-        self.setSessionData()
-        self.sessionCreate(ch)
+        data = {'grant_type':'refresh_token','refresh_token':session['refresh_token']}
+        ch = self.curl_exec("oauth2/token", data)
+        self.set_session_data()
+        self.session_create(ch)
 
     # This will do the authentication
     def authenticate(self):
@@ -39,17 +35,17 @@ class SSO(Authentication):
         if request.args.get('code') is None:
             # First we need a token for our app
             data['grant_type'] = "client_credentials"
-            accessToken = self.curlExec("oauth2/token",data).json()['access_token']
-            data['access_token'] = accessToken
-            res = self.curlExec("oauth2/resource", data)
+            access_token = self.curl_exec("oauth2/token", data).json()['access_token']
+            data['access_token'] = access_token
+            res = self.curl_exec("oauth2/resource", data)
             # If we have our token, we will ask for the user's access_code (this will throw the user to the login page)
             if res.status_code == 200:
                 # Hash some data about the user
-                userHash = str(hashlib.sha1(str(request.remote_addr).encode('utf-8') + str(request.headers.get('User-Agent')).encode('utf-8')).hexdigest())
-                # Get the REST API's URL
-                URL = self.hostname + "site/login?response_type=code&client_id=" + self.username + "&state=" + userHash + "&scope=" + self.scope
+                user_hash = str(hashlib.sha1(str(request.remote_addr).encode('utf-8') + str(request.headers.get('User-Agent')).encode('utf-8')).hexdigest())
+                # Get the REST API's url
+                url = self.hostname + "site/login?response_type=code&client_id=" + self.username + "&state=" + user_hash + "&scope=" + self.scope
                 # Redirect the user to the login page
-                return URL
+                return url
             else:
                 # If something bad happens, some error page should be shown.
                 raise Exception("Login Error")
@@ -57,11 +53,11 @@ class SSO(Authentication):
             # If we have the access code, get the user's access token
             data['grant_type'] = "authorization_code"
             data['code'] = request.args.get('code')
-            ch = self.curlExec("oauth2/token",data)
+            ch = self.curl_exec("oauth2/token", data)
             if ch.status_code == 200:
                 # If we have the access_token, create the user's session, and get the user's data
-                self.sessionCreate(ch)
-                self.setSessionData()
+                self.session_create(ch)
+                self.set_session_data()
                 # Finally redirect the user to the home page
                 return True
             else:
@@ -69,7 +65,7 @@ class SSO(Authentication):
                 raise Exception("Login Error")
 
     # Just a simple requests.post wrapper
-    def curlExec(self,urlPart, data):
+    def curl_exec(self, urlPart, data):
         if urlPart != "oauth2/resource":
             ret = requests.post(self.hostname + urlPart,data,auth=HTTPBasicAuth(self.username,self.password))
             return ret
@@ -78,7 +74,7 @@ class SSO(Authentication):
             return ret
 
     # This creates the user session from the data got from the SSO server.
-    def sessionCreate(self,ch):
+    def session_create(self, ch):
         data = json.loads(ch.text)
         session['access_token']     = data['access_token']
         session['refresh_token']    = data['refresh_token']
@@ -88,18 +84,18 @@ class SSO(Authentication):
         session['start']            = datetime.datetime.now()
 
     # This adds some additional data to the user session
-    def setSessionData(self):
-        data = self.getData()
+    def set_session_data(self):
+        data = self.get_data()
         session['internal_id'] = data['internal_id']
         session['displayName'] = data['displayName']
 
     # This asks for additional data about the user, such as username, the user's name, email etc..
-    def getData(self):
+    def get_data(self):
         url = self.hostname + "api/profile/?access_token=" + session['access_token']
         ret = requests.get(url)
-        if( ret != None and ret.status_code == 200):
+        if ret is not None and ret.status_code == 200:
             data = ret.json()
-            if data != None:
+            if data is not None:
                 return data
         return None
 
