50e06a1cda62581114b170c8fb9c212dc978f975
kovax <kovacsdanielhun@gmail.com>
2016-10-23 16:45:57 +0200

    Phones page for the front end is added.

--- ServeYourself/static/js/page.js
+++ ServeYourself/static/js/page.js
@@ -14,6 +14,7 @@ function loadHTMLElements(callback) {
         loadHTMLElementsReady(callback);
         navReady()
     });
+    $('#content').load('sites/' + GetURLParameter('site') + '.html')
 
 };
 
---
+++ ServeYourself/static/js/phones.js
@@ -0,0 +1,3 @@
+/**
+ * Created by kovax on 2016. 10. 23..
+ */
